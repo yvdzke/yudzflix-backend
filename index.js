5a356219a34e2c3c5842cc5bbac06048194ff6bf
yvdzke . <yudzke@gmail.com>
2026-01-12 00:54:10 +0700

    edit cors issues

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors"); // <--- INI PENTING BUAT FRONTEND
+const cors = require("cors");
 const authRoutes = require("./src/routes/authRoutes");
 const userRoutes = require("./src/routes/userRoutes");
 const favoriteRoutes = require("./src/routes/favoriteRoutes");
@@ -8,18 +8,36 @@ const favoriteRoutes = require("./src/routes/favoriteRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors()); // Izinkan Frontend akses Backend ini
+const allowedOrigins = [
+  "http://localhost:5173",
+  // "http://192.168.1.10:5173",
+  "https://yudzflix.vercel.app/",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.log("Blocked by CORS:", origin); // Buat debug di log render
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Enable cookies and auth headers
+  })
+);
+// ---------------------------------------------
+
 app.use(express.json());
 
 // Routes
-
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/favorites", favoriteRoutes);
 
 app.get("/", (req, res) => {
-  res.send("<h1>Yvdzflix Backend Connected</h1>");
+  res.send("<h1>Yvdzflix Backend Connected & Secured 🔒</h1>");
 });
 
 // Start Server
