119a77c0fe04f8404ff003e1d1a970a4da216cea
yvdzke . <yudzke@gmail.com>
2026-01-12 21:24:46 +0700

    change port localhost

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
-  "http://localhost:5173",
-  "http://192.168.1.10:5173",
+  "http://localhost:5174",
+  "http://192.168.1.10:5174",
   "https://yudzflix.vercel.app",
 ];
 
